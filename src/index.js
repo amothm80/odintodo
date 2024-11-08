ee4542402c598bdf29d7e4ea26df043fc5cb8938
amothm80 <amo.1980@gmail.com>
2024-11-08 10:48:58 +0300

    UI cont

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import './styles.css';
 import userIcon from './assets/user.png'
+import notifOn from './assets/notif-on.png';
+import notifOff from './assets/notif-off.png';
+import addProject from './assets/plus.png';
