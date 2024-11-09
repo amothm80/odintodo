cc4d08086edfd8dd60d036877b74f59256022e84
amothm80 <amo.1980@gmail.com>
2024-11-09 10:05:57 +0300

    logic: todo handler

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import './styles.css';
+import { todoHandler } from './todo.js';
 import userIcon from './assets/user.png'
 import notifOn from './assets/notif-on.png';
 import notifOff from './assets/notif-off.png';
 import addProject from './assets/plus.png';
+
+// let tasks = [{title:"test title", project:"test project", description:"test descrption", dueDate:"12-12-2024", priority:"high"},
+//     {title:"test title2", project:"test project2", description:"test descrption2", dueDate:"01-01-2025", priority:"low"}
+// ]
+// let projects =[{title: "test project"},{title: "test project2"}]
+
+
+// window.todoHandler = todoHandler;
+
+//filtering todo.readTodoTasks().filter((task)=>task.getTodoTask().taskProject == "test project")
--- src/todo.js
+++ src/todo.js
@@ -1,20 +1,66 @@
-function toDoTask(title, project, description, dueDate, priority){
+function todoTaskF(title, project, description, dueDate, priority){
     let taskTitle = title;
     let taskProject = project;
     let taskDescription = description;
     let taskDueDate = dueDate;
-    let taskPriority = [priority];
-    function getToDoTask() {return {taskTitle,taskProject,taskDescription, taskDueDate,taskPriority}};
+    let taskPriority = priority;
+    function getTodoTask() {return {taskTitle,taskProject,taskDescription, taskDueDate,taskPriority}};
     function modifyTaskTitle(title) {taskTitle = title;};
     function modifyTaskDescription(description) {taskDescription = description};
     function modifyTaskDueDate(dueDate) { taskDueDate = dueDate};
     function modifyTaskPriority(priority) {taskPriority = priority};
-    return {getToDoTask, modifyTaskTitle, modifyTaskDescription, modifyTaskDueDate, modifyTaskPriority};
+    function modifyTodoTask(title, project, description, dueDate, priority){
+        taskTitle = title;
+        taskProject = project;
+        taskDescription = description;
+        taskDueDate = dueDate;
+        taskPriority = priority;
+    }
+    return {getTodoTask, modifyTaskTitle, modifyTaskDescription, modifyTaskDueDate, modifyTaskPriority, modifyTodoTask};
 }
 
-function todoProject(todoProjectTitle){
-    let projectTitle = todoProjectTitle;
-    function getToDoProject(){return projectTitle};
+function todoProjectF(title){
+    let projectTitle = title;
+    function getTodoProject(){return projectTitle};
     function modifyProjectTitle(title){ projectTitle = title; }
-    return{getToDoProject, modifyProjectTitle}
+    function modifyTodoProject(title){
+        projectTitle = title;
+    }
+    return{getTodoProject, modifyProjectTitle, modifyTodoProject}
 }
+
+// let tasks = [{taskTitle:"test title", taskProject:"test project", taskDescription:"test descrption", taskDueDate:"12-12-2024", taskPriority:"high"}]
+// let projects =[{projectTitle: "test project"}]
+
+export const todoHandler = function(tasks, projects){
+    let todoTasks = [];
+    let todoProjects = [];
+    for (const task of tasks){
+        //the intial read from storage
+        addTodoTask(task.title, task.project, task.description, task.dueDate, task.priority)
+    }
+
+    for (const project of projects){
+        //the intial read from storage
+        addTodoProject(project.title);
+    }
+    // let todoTasks = [];
+    // let todoProjects = [];
+
+    function addTodoTask(title, project, description, dueDate, priority){todoTasks.push(todoTaskF(title, project, description, dueDate, priority))};
+    function addTodoProject(title){todoProjects.push(todoProjectF(title))};
+    function readTodoTasks(){return todoTasks}
+    function readTodoProjects(){return todoProjects}
+    function modifyTodoTask(index,title, project, description, dueDate, priority ){
+        todoTasks(index).modifyTodoTask(title, project, description, dueDate, priority);
+    }
+    function modifyTodoProject(index, title){
+        todoProjects(index).modifyTodoProject(title)
+    }
+    function deleteTodoTask(index){todoTasks.splice(index,1);};
+    function deleteTodoProject(index){todoProjects.splice(index,1);};
+
+    return {
+        addTodoTask,addTodoProject,readTodoTasks, readTodoProjects, modifyTodoTask, modifyTodoProject, deleteTodoTask, deleteTodoProject
+    }
+};
